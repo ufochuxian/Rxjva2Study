2ec9a00706aa7b40ca7a36cdf1a8e8812a84247c
ufochuxian <chen853409335@gmail.com>
2021-06-24 20:55:21 +0800

    first commit

---
+++ app/src/main/java/com/eric/rxjava/Observable.java
@@ -0,0 +1,15 @@
+package com.eric.rxjava;
+
+/**
+ * @Author: chen
+ * @datetime: 2021/6/24
+ * @desc:
+ */
+class Observable {
+
+    //1. 创建被观察者
+    public static Observable create(Observable observable) {
+        return observable;
+    }
+
+}
